c0d0f0c54489cb622886a64533c3bf9dac69fda1
Caleb Stokka <caleb.stokka@gmail.com>
2016-06-09 18:56:04 -0700

    add basic number input to html, add specs to readme, add ability to handle 3,5,15 to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
-/*This file is for your custom js.  All yours*/
+//Business Logic
+var array = []
+function pingpong(n) {
+  // debugger;
+  for (var i = 1; i <= n; i++) {
+    if (!(i%15)) {
+      array.push("pingpong");
+    }
+    else if (!(i%3)) {
+      array.push("ping");
+    }
+    else if (!(i%5)) {
+      array.push("pong");
+    }
+    else {
+      array.push(i);
+    }
+  }
+  console.log(array);
+  return array;
+}
+//User Interface Logic
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var inputText = $("#input").val();
+    var result = pingpong(inputText);
+    $("#result").text(result);
+  });
+});
